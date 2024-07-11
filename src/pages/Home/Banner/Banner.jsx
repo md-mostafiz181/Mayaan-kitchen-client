6cb011d3fc771dc32b0ab0eb2bd0c9afc134d468
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-07-11 09:30:18 +0600

    added banner button and make it more attractive

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import "./Banner.css";
+import Container from "../../../components/Container/Container";
+import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
+
+const Banner = () => {
+  return (
+    <div id="banner-part">
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide className="banner1">
+          <div className="overlay">
+            <Container>
+              <div className="banner-text mt-[340px]">
+                <h1 className="lg:text-5xl sm:tex-3xl  font-bold uppercase text-orange-400 secondaryFont text-center">
+                  Welcome To Mayaan Kitchen
+                </h1>
+                <h1 className="lg:text-5xl sm:tex-3xl  font-bold uppercase   text-white primaryFont mt-4 text-center">
+                  Fast Food
+                </h1>
+
+                <h3 className="lg:text-3xl sm:text-2xl font-bold capitalize text-orange-400 mt-5 secondaryFont text-center">
+                  Special Menu
+                </h3>
+                <p className="capitalize  lg:text-[20px] sm:text-[10px] font-normal   primaryFont mt-4 px-10 text-white text-center">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit
+                  minus a quam numquam, nihil voluptas odio nulla at eligendi
+                  aperiam.Lorem ipsum dolor sit, amet consectetur adipisicing
+                  elit. Odit minus a quam numquam, nihil voluptas odio nulla at
+                  eligendi aperiam.
+                </p>
+
+                <div className="banner-icon flex justify-center  mt-5">
+                  <div className="youtube w-[40px] h-[40px] flex justify-center items-center bg-orange-500 rounded-full mr-2">
+                    {" "}
+                    <FaYoutube className="text-white icon"></FaYoutube>{" "}
+                  </div>
+                  <div className="twitter w-[40px] h-[40px] flex justify-center items-center bg-orange-500 rounded-full mr-2">
+                    <FaTwitter className="text-white icon"></FaTwitter>
+                  </div>
+                  <div className="facebook w-[40px] h-[40px] flex justify-center items-center bg-orange-500 rounded-full mr-2">
+                    <FaFacebook className="text-white icon"></FaFacebook>
+                  </div>
+                  <div className="instagram w-[40px] h-[40px] flex justify-center items-center bg-orange-500 rounded-full mr-2">
+                    <FaInstagram className="text-white icon"></FaInstagram>
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex justify-center items-center mt-5">
+                <div className="banner-button ">
+                  <button className="primaryFont font-normal uppercase ">
+                    Order Now
+                  </button>
+                </div>
+                <div className="banner-button ms-5">
+                  <button className="primaryFont font-normal uppercase ">
+                    Learn More
+                  </button>
+                </div>
+              </div>
+            </Container>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="banner2 overlay">
+          <div className="overlay"></div>
+        </SwiperSlide>
+        <SwiperSlide className="banner3 overlay">
+          <div className="overlay"></div>
+        </SwiperSlide>
+        <SwiperSlide className="banner4 overlay">
+          <div className="overlay"></div>
+        </SwiperSlide>
+        <SwiperSlide className="banner5 overlay">
+          <div className="overlay"></div>
+        </SwiperSlide>
+        <SwiperSlide className="banner6 overlay">
+          <div className="overlay"></div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,56 +1,84 @@
-import React from 'react';
-import Container from '../../../components/Container/Container';
-import "./Navbar.css"
+import React from "react";
+import Container from "../../../components/Container/Container";
+import "./Navbar.css";
 
 const Navbar = () => {
-
-    const navOptions= <div className='nav-bar lg:flex '>
-    <li><a className='uppercase lg:text-[15px] sm:text-[10px] font-bold'>Home</a></li>
-    <li><a className='uppercase lg:text-[15px] sm:text-[10px] font-bold'>Contact</a></li>
-    <li><a className='uppercase lg:text-[15px] sm:text-[10px] font-bold'>our menu</a></li>
-    <li><a className='uppercase lg:text-[15px] sm:text-[10px] font-bold'>shop</a></li>
-    <li><a className='uppercase lg:text-[15px] sm:text-[10px] font-bold'>dashboard</a></li>
+  const navOptions = (
+    <div className="nav-bar lg:flex ">
+      <li>
+        <a className="uppercase lg:text-[15px] sm:text-[10px] font-bold">
+          Home
+        </a>
+      </li>
+      <li>
+        <a className="uppercase lg:text-[15px] sm:text-[10px] font-bold">
+          Contact
+        </a>
+      </li>
+      <li>
+        <a className="uppercase lg:text-[15px] sm:text-[10px] font-bold">
+          our menu
+        </a>
+      </li>
+      <li>
+        <a className="uppercase lg:text-[15px] sm:text-[10px] font-bold">
+          shop
+        </a>
+      </li>
+      <li>
+        <a className="uppercase lg:text-[15px] sm:text-[10px] font-bold">
+          dashboard
+        </a>
+      </li>
     </div>
-    return (
-        <div className='bg-orange-300'>
-           <Container>
-           <div className="navbar ">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h8m-8 6h16" />
-        </svg>
-      </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-green-400 rounded-box  mt-3">
-        {navOptions}
-      </ul>
-    </div>
-    <a className="lg:text-xl sm:text-[10px] lg:uppercase font-normal  ">Mayaan Kitchen</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      {navOptions}
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Login</a>
-  </div>
-</div>
-           </Container>
+  );
+  return (
+    <div className="bg-orange-300">
+      <Container>
+        <div className="navbar ">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-green-400 rounded-box  mt-3"
+              >
+                {navOptions}
+              </ul>
+            </div>
+            <a className="lg:text-xl sm:text-[10px] lg:uppercase font-normal  ">
+              Mayaan Kitchen
+            </a>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">{navOptions}</ul>
+          </div>
+          <div className="navbar-end">
+            <a className="btn">Login</a>
+          </div>
         </div>
-    );
+      </Container>
+    </div>
+  );
 };
 
 export default Navbar;
