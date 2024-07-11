e3714908d4d2eff156e45999bd62c5be3779ad66
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-07-11 12:09:46 +0600

    added slider and make it also responsive

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SectionTitle = ({subHeading, heading}) => {
     return (
         <div className='lg:w-3/12 text-center mt-2 mx-auto'>
-            <p className='secondaryFont lg:text-xl text-[#f97316] font-bold'>---{subHeading}---</p>
+            <p className='secondaryFont lg:text-xl text-[#f97316] font-bold'>....{subHeading}....</p>
             <h1 className='primaryFont lg:text-2xl text-[#f97316] font-normal uppercase border-y-4 py-4 mt-2'>{heading}</h1>
         </div>
     );
--- src/pages/Home/Order/Order.jsx
+++ src/pages/Home/Order/Order.jsx
@@ -1,19 +1,121 @@
+
 import React from "react";
-import "./Order.css";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import img1 from "../../../assets/dessert.jpg";
+import img2 from "../../../assets/drinks.jpg";
+import img3 from "../../../assets/pizza.jpg";
+import img4 from "../../../assets/salad.jpg";
+import img5 from "../../../assets/soup.jpg";
+import Container from "../../../components/Container/Container";
+import "./Order.css";
 
 const Order = () => {
   return (
-    <div>
-      <section>
+    <div id="order-part">
+      <section className="mb-4">
         <SectionTitle
           subHeading={"From 11 a.m to 10 p.m"}
           heading={"Order Online"}
         ></SectionTitle>
       </section>
-      <h1>this is order page</h1>
+
+      <div>
+        <Container>
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={30}
+            breakpoints={{
+                // when window width is >= 330px and <= 640px
+                330: {
+                  slidesPerView: 1,
+                  spaceBetween: 20,
+                },
+                // when window width is >= 640px
+                640: {
+                  slidesPerView: 4,
+                  spaceBetween: 30,
+                },
+                // when window width is >= 768px
+                768: {
+                  slidesPerView: 2,
+                  spaceBetween: 30,
+                },
+                // when window width is >= 1024px
+                1024: {
+                  slidesPerView: 3,
+                  spaceBetween: 30,
+                },
+                // when window width is >= 1280px
+                1280: {
+                  slidesPerView: 4,
+                  spaceBetween: 30,
+                },
+              }}
+            
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <div className="img-box h-[400px] bg-orange-400">
+                <img src={img1} alt="" />
+                <div className="overlay">
+                  <div className="overlayText">
+                    <h1 className="text-4xl text-white secondaryFont">Dessert</h1>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="img-box h-[400px] bg-orange-400">
+                <img src={img2} alt="" />
+                <div className="overlay">
+                  <div className="overlayText">
+                    <h1 className="text-4xl text-white secondaryFont">Drinks</h1>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="img-box h-[400px] bg-orange-400">
+                <img src={img3} alt="" />
+                <div className="overlay">
+                  <div className="overlayText">
+                    <h1 className="text-4xl text-white secondaryFont">Pizza</h1>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="img-box h-[400px] bg-orange-400">
+                <img src={img4} alt="" />
+                <div className="overlay">
+                  <div className="overlayText">
+                    <h1 className="text-4xl text-white secondaryFont">Salad</h1>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="img-box h-[400px] bg-orange-400">
+                <img src={img5} alt="" />
+                <div className="overlay">
+                  <div className="overlayText">
+                    <h1 className="text-4xl text-white secondaryFont">Soup</h1>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </Container>
+      </div>
     </div>
   );
 };
 
 export default Order;
+
+
