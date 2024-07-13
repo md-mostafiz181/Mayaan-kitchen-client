24280f675780107de02743e59d888ccf1c087486
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-07-13 11:35:33 +0600

    added parallax

--- src/pages/Home/Latest/Latest.jsx
+++ src/pages/Home/Latest/Latest.jsx
@@ -11,7 +11,7 @@ import Container from "../../../components/Container/Container";
 
 const Latest = () => {
   return (
-    <div id="Latest-part">
+    <div id="Latest-part" className="bg-fixed">
       <Container>
         <div className="section-title">
           <section className="mt-4">
@@ -125,3 +125,54 @@ const Latest = () => {
 };
 
 export default Latest;
+
+
+// import React from "react";
+// import { Parallax } from "react-parallax";
+// import "./Latest.css";
+// import img1 from "../../../assets/latest1.jpg";
+// import img2 from "../../../assets/latest2.jpg";
+// import img3 from "../../../assets/latest3.jpg";
+// import img4 from "../../../assets/latest4.jpg";
+// import img5 from "../../../assets/latest5.jpg";
+// import img6 from "../../../assets/latest6.jpg";
+// import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+// import Container from "../../../components/Container/Container";
+
+// const Latest = () => {
+//   return (
+//     <div id="Latest-part" className="bg-fixed">
+//       <Container>
+//         <div className="section-title">
+//           <section className="mt-4">
+//             <SectionTitle subHeading={"Blog"} heading={"Latest"}></SectionTitle>
+//           </section>
+//         </div>
+
+//         <div className="latest-main grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-3 mt-5">
+//           {[img1, img2, img3, img4, img5, img6].map((img, index) => (
+//             <Parallax bgImage={img} strength={500} key={index}>
+//               <div className="latest-box1 w-full h-[400px] border relative">
+//                 <div className="overlay"></div>
+//                 <div className="content absolute inset-0 flex flex-col justify-center items-center text-white">
+//                   <h1 className="secondaryFont lg:text-3xl sm:text-2xl mt-20 text-center">
+//                     {["Pizza", "Burger", "Pasta", "Dessert", "Grapes", "Snacks"][index]}
+//                   </h1>
+//                   <p className="text-center lg:text-2xl sm:text-xl font-bold primaryFont mt-7">
+//                     Delicious {["pizza", "burger", "pasta", "dessert", "grapes", "snacks"][index]} is like a slice of happiness, with each bite
+//                   </p>
+//                   <h5 className="secondaryFont text-xl text-center uppercase tracking-widest mt-7">
+//                     Mayaan Kitchen
+//                   </h5>
+//                 </div>
+//               </div>
+//             </Parallax>
+//           ))}
+//         </div>
+//       </Container>
+//     </div>
+//   );
+// };
+
+// export default Latest;
+
