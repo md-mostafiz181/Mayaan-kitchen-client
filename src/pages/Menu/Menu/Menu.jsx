4e90bfc3f332e927c442b379ef13e6f9de72fb4e
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-08-13 08:01:58 +0600

    added react parallax and also added dynamic cover img

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from "../../../assets/Menu.bg.jpg"
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
+import dessertBg from "../../../assets/dessertBg.jpg"
+import pizzaBg from "../../../assets/pizzaBg.jpg"
+import saladBg from "../../../assets/saladBg.jpg"
+import soupBg from "../../../assets/soupBg.jpg"
 
 const Menu = () => {
     return (
@@ -8,6 +15,17 @@ const Menu = () => {
             <Helmet>
                 <title>Kitchen || Our Menu</title>
             </Helmet>
+
+            <Cover img={menuImg} title="our menu" subTitle="would you like to try dish?"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={dessertBg} title="dessert" subTitle="would you like to try dish?"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={pizzaBg} title="pizza" subTitle="would you like to try dish?"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={saladBg} title="salad" subTitle="would you like to try dish?"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={soupBg} title="soup" subTitle="would you like to try dish?"></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
